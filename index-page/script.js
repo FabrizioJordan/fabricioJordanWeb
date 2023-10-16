46e8f8cd49045996aaa5dc95c0d5a6b4e13f76aa
Fabrizio Jordan <fabriorange28@gmail.com>
2023-10-16 18:32:39 -0300

    Header HECHO

---
+++ index-page/script.js
@@ -0,0 +1,11 @@
+// ABRO BOTON INFORMACION DISCLAIMER
+
+const btnInfo1 = document.getElementById('btn__info-1');
+const textInfo1 = document.getElementById('text__Info1');
+btnInfo1.addEventListener('click', clickBtnInfo1);
+
+function clickBtnInfo1() {
+  textInfo1.classList.toggle("hidden");
+  btnInfo1.classList.toggle("!bg-red-600");
+  btnInfo1.classList.toggle("childrenRotated");
+}
